70817bdc6042ffbeab3b2015d23918731e3b7b97
MrSmokyOnion <smokyonion@kakao.com>
2024-05-27 11:44:06 +0900

    - 코드 삭제되서 에러뜬거 다시 추가했습니다. - test_ui_SkillCoolDown 씬 : 쿨타임 UI 템플릿 만들었습니다.

---
+++ Assets/02_Scripts/UI/CoolDownSlider.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownSlider : MonoBehaviour
+{
+    Slider ui_Slider;
+
+    private float maxTime;
+    private bool pauseTrigger;
+
+    private void Start()
+    {
+        ui_Slider = GetComponent<Slider>();
+    }
+
+    public void Initialize(int _maxValue)
+    {
+        ui_Slider.maxValue = maxTime = _maxValue;
+        ui_Slider.value = 0;
+        pauseTrigger = false;
+    }
+
+    public void StartCoolDown()
+    {
+        StartCoroutine(CoolDownTimer());
+    }
+
+    public void PauseCoolDown()
+    {
+        pauseTrigger = true;
+    }
+
+    public void ResumeCoolDown()
+    {
+        pauseTrigger = false;
+    }
+
+    private void FinishCoolDown()
+    {
+        //TODO : 스킬 사용하는 코드 추가해야 함.
+    }
+
+    private IEnumerator CoolDownTimer()
+    {
+        float _timer = 0;
+
+        while (true)
+        {
+            yield return new WaitForEndOfFrame();
+            if (pauseTrigger)
+            {
+                continue;
+            }
+
+            _timer += Time.deltaTime;
+            if( _timer > maxTime) 
+            {
+                FinishCoolDown();
+                continue;   //쿨타임 UI가 알아서 작동하게 할 것인가, 아니면 다른곳에서 이 코루틴을 반복실행하게 하는 코드가 필요.
+            }
+        }
+    }
+}
